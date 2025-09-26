70e32cc8b90d56f80f6dcdec44e3ebaec633b578
Thomas Hewatt <116681336+thewatt-7@users.noreply.github.com>
2025-09-26 11:22:01 -0400

    database test

---
+++ src/migrations/20250926_create_test_table.js
@@ -0,0 +1,30 @@
+import db from '../config/db.js';
+
+export async function up() {
+  try {
+    await db.query(`
+      CREATE TABLE IF NOT EXISTS test (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) UNIQUE NOT NULL, 
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password VARCHAR(100) UNIQUE, 
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `);
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// If created and we don't need anymore use down function to drop table 
+export async function down() { 
+  try {
+    await db.query('DROP TABLE IF EXISTS test');
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// Specify fucntion here
+// command to run: node --env-file=.env src/migrations/20250926_create_test_table.js
+up()
---
+++ src/migrations/20250926_create_users_table.js
@@ -0,0 +1,28 @@
+import db from '../config/db.js';
+
+export async function up() {
+  try {
+    await db.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) UNIQUE NOT NULL, 
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `);
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// If created and we don't need anymore use down function to drop table 
+export async function down() { 
+  try {
+    await db.query('DROP TABLE IF EXISTS users');
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// Specify fucntion here
+// command to run: node --env-file=.env src/migrations/20250926_create_users_table.js
+up()
