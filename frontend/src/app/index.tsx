7291ad251522a313fc15e28ea4c89e13a7e0dbaa
Thomas Hewatt <116681336+thewatt-7@users.noreply.github.com>
2025-10-20 14:43:40 -0400

    Register page functions

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -3,6 +3,7 @@ import { Link } from "expo-router";
 import { getUsers } from "./services/userService";
 import { useQuery } from "@tanstack/react-query";
 import LoginPage from "./login";
+import AboutScreen from "./about";
 // 4:02:43
 
 export default function HomeScreen() {
